20fb10f4d75430fa8f2ff6c967ba0ac9e53d5eec
Teyeb <hussenab@gmail.com>
2021-10-27 00:46:21 +0300

    array issue fixed

--- src/redux/sagas/country.ts
+++ src/redux/sagas/country.ts
@@ -6,4 +6,5 @@ function* doSomethingWhenAddingCountry(action: AddCountryAction) {
   yield console.log(action)
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [takeLatest(ADD_COUNTRY, doSomethingWhenAddingCountry)]
